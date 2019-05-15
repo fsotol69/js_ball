dfb93168429afe2eabfe75cecee66b77260f4ca3
fsotol69 <fsotol69@gmail.com>
2019-05-15 23:05:13 +0200

    Final

--- main.js
+++ main.js
@@ -4,28 +4,41 @@ let idInterval;
 let ctx = canvas.getContext('2d');
 
 let x = 50;
-let y = 150;
-let vel = 2;
-let date = new Date();
-let time1 = 0;
+let y = 100;
+// let ang = -45;
+// let angulo = (ang * Math.PI) / 180;
+let m = 0.2;
+let incrX = 5;
+let incrY = 5 * m;
 
-canvas.width = 500;
+let x_min = 10;
+let x_max = 480;
+let y_min = 10;
+let y_max = 490;
+
+canvas.width = 490;
 canvas.height = 500;
 
-function calculaPosicion() {
-  let date2 = new Date();
-  let time2 = date2.getMilliseconds();
-
-  console.log(`time1 = ${time1}  time2 = ${time2}`);
-
-  let incr = (time2 - time1) / 1000;
-  x = x + vel * incr;
-  y = y + 0.5 * 9.81 * incr * incr;
-
-  console.log(`x = ${x}  y = ${y}`);
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function pintarCirculo() {
+function calculaPosicion() {
+  if (x + incrX > x_max || x + incrX < x_min) {
+    incrX *= -1.1;
+    incrY *= 1.2;
+  } else if (y + incrY < y_min || y + incrY > y_max) {
+    incrX *= (1/1.2);
+    incrY *= -(1/1.3);
+  }
+  x += incrX;
+  y += incrY;
+}
+
+function pintaCirculo() {
+  calculaPosicion();
+  clearCanvas();
+
   ctx.strokeStyle = 'greenyellow';
   ctx.lineWidth = 20;
 
@@ -33,16 +46,10 @@ function pintarCirculo() {
   ctx.arc(x, y, 10, 0, 2 * Math.PI);
   ctx.fill();
   ctx.stroke();
-
-  calculaPosicion();
-  idInterval = setInterval(pintarCirculo, 1000);
 }
 
 function start() {
-  time1 = date.getMilliseconds();
-  //   calculaPosicion();
-  //pintarCirculo();
-  //setInterval(dibuja, 1000);
+  idInterval = setInterval(pintaCirculo, 10);
 }
 
 function stop() {
